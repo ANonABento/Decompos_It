01e549e979769b9852cff836e0893898d321134c
ANonABento <kevin.jiang53@gmail.com>
2023-10-10 13:10:07 -0400

    reset no camera2

--- app/src/main/java/com/example/decomposeit/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/decomposeit/ui/home/HomeFragment.java
@@ -1,12 +1,7 @@
 package com.example.decomposeit.ui.home;
 
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -17,27 +12,9 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.decomposeit.databinding.FragmentHomeBinding;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraMetadata;
-import androidx.core.app.ActivityCompat;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Surface;
-import java.util.Arrays;
-import android.content.Context;
-import com.example.decomposeit.R;
-
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-    private CameraCaptureSession myCameraCaptureSession;
-    private String myCameraID;
-    private CameraManager myCameraManager;
-    private CameraDevice myCameraDevice;
-    private TextureView myTextureView;
-    private CaptureRequest.Builder myCaptureRequestBuilder;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,12 +26,6 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
-        // Initialize the camera here
-        myTextureView = root.findViewById(R.id.textureView);
-        myCameraManager = (CameraManager) requireContext().getSystemService(Context.CAMERA_SERVICE);
-        openCamera(); // Call the method to open the camera
-
         return root;
     }
 
@@ -62,78 +33,5 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    private CameraDevice.StateCallback myStateCallBack = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice camera) {
-            myCameraDevice = camera;
-        }
-
-        @Override
-        public void onDisconnected(@NonNull CameraDevice camera) {
-            myCameraDevice.close();
-        }
-
-        @Override
-        public void onError(@NonNull CameraDevice camera, int error) {
-            myCameraDevice.close();
-            myCameraDevice = null;
-        }
-    };
-
-    private void openCamera() {
-        try {
-            myCameraID = myCameraManager.getCameraIdList()[0];
-
-            ActivityCompat.requestPermissions(requireActivity(),
-                    new String[]{
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.RECORD_AUDIO
-                    },
-                    PackageManager.PERMISSION_GRANTED);
-
-            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            myCameraManager.openCamera(myCameraID, myStateCallBack, null);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    //maybe redundant
-    public void cameraPreview(View view){
-        SurfaceTexture mySurfaceTexture = myTextureView.getSurfaceTexture();
-        Surface mySurface = new Surface(mySurfaceTexture);
-
-        try {
-            myCaptureRequestBuilder = myCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            myCaptureRequestBuilder.addTarget(mySurface);
-
-            myCameraDevice.createCaptureSession(Arrays.asList(mySurface), new CameraCaptureSession.StateCallback() {
-                        @Override
-                        public void onConfigured(@NonNull CameraCaptureSession session) {
-                            myCameraCaptureSession = session;
-                            myCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-                            try {
-                                myCameraCaptureSession.setRepeatingRequest(myCaptureRequestBuilder.build(), null, null);
-                            } catch (CameraAccessException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-
-                        }
-                    }, null
-            );
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
     }
 }
