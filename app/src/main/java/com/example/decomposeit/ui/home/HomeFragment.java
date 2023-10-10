61f9fe6fc4ca310f1bdb2a345394845df574bb12
ANonABento <kevin.jiang53@gmail.com>
2023-10-10 13:10:07 -0400

    reset no camera2

--- app/src/main/java/com/example/decomposeit/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/decomposeit/ui/home/HomeFragment.java
@@ -26,7 +26,6 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
         return root;
     }
 
