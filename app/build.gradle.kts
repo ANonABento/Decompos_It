5d59e7a0ec731af862fb5e666ba041ab2215fe37
ANonABento <kevin.jiang53@gmail.com>
2023-10-12 13:33:43 -0400

    camera WORKSSSSSSSSSSSSSS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.decomposeit"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,6 +33,7 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
 dependencies {
