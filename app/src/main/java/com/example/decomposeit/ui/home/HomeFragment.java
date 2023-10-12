505dbcc8cf01fa65ded50270f0ee1a03a06ac91e
ANonABento <kevin.jiang53@gmail.com>
2023-10-12 13:33:43 -0400

    camera WORKSSSSSSSSSSSSSS

--- app/src/main/java/com/example/decomposeit/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/decomposeit/ui/home/HomeFragment.java
@@ -1,45 +1,65 @@
 package com.example.decomposeit.ui.home;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.camera.core.CameraInfoUnavailableException;
+import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
+import androidx.navigation.Navigation;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import com.example.decomposeit.databinding.ActivityMainBinding;
+import com.example.decomposeit.databinding.FragmentHomeBinding;
 import com.example.decomposeit.R;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+
 
 public class HomeFragment extends Fragment {
 
-    private TextureView myTextureView;
     private Button imageCaptureButton;
-
-    // CameraX's variables
     private ImageCapture imageCapture;
+    private TextureView viewFinder;
     private ExecutorService cameraExecutor;
+    private static final String TAG = "HomeFragment";
+    private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot(); // Corrected this line
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -53,7 +73,7 @@ public class HomeFragment extends Fragment {
         imageCaptureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                takePhoto();
+                takePhoto(view);
             }
         });
 
@@ -62,14 +82,85 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    private void takePhoto() {
-        // Implement your take photo logic here
-        // For example, start a camera activity, show a dialog, etc.
+    private void takePhoto(View view) {
+        // Get a stable reference of the modifiable image capture use case
+        ImageCapture imageCapture = this.imageCapture;
+        if (imageCapture == null) {
+            return;
+        }
+
+        // Create a time-stamped name and MediaStore entry
+        String name = new SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis());
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+        }
+
+        // Create output options object which contains file + metadata
+        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
+                requireContext().getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+                .build();
+
+        // Set up image capture listener, which is triggered after the photo has been taken
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(requireContext()), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onError(@NonNull ImageCaptureException exc) {
+                Toast.makeText(requireContext(), "Photo capture failed: " + exc.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults output) {
+                String msg = "Photo capture succeeded: " + output.getSavedUri();
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void startCamera() {
-        // Implement your camera initialization logic here
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
+
+        cameraProviderFuture.addListener(() -> {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            ProcessCameraProvider cameraProvider;
+            try {
+                cameraProvider = cameraProviderFuture.get();
+
+                // Initialize viewFinder from the existing binding object
+                PreviewView viewFinder = binding.viewFinder; // Use the existing binding object instead of inflating a new one
+
+                // Preview
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
+
+                // Select back camera as a default
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+
+                try {
+                    // Unbind use cases before rebinding
+                    cameraProvider.unbindAll();
+
+                    // Bind use cases to the camera
+                    cameraProvider.bindToLifecycle(getViewLifecycleOwner(), cameraSelector, preview);
+                } catch (Exception exc) {
+                    Log.e(TAG, "Use case binding failed", exc);
+                }
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(requireContext()));
     }
+
+
+
+
+
 
     private void requestPermissions() {
         activityResultLauncher.launch(REQUIRED_PERMISSIONS);
@@ -116,4 +207,6 @@ public class HomeFragment extends Fragment {
             Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO
     };
+
+    private static final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
 }
